ef9a68a76788d6b9b6017f1c88fdbbf5576e247d
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-03-17 21:39:00 -0300

    primeiro commit

---
+++ src/test/java/br/com/vfs/demomongodb/DemoMongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.vfs.demomongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoMongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
